7906d375d23ffbf2891bf18992e3a1df467ed951
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-02-07 12:04:59 +0800

    Prepare RESUME_STAGE_INFO text

--- src/main/java/view/swing/ViewConstants.java
+++ src/main/java/view/swing/ViewConstants.java
@@ -7,13 +7,33 @@ public class ViewConstants {
     public static final int WINDOW_WIDTH_PIXELS = 1280;
     public static final int WINDOW_HEIGHT_PIXELS = 720;
     public static final Font FONT_LARGE = new Font(Font.DIALOG, Font.PLAIN, 26);
+    public static final Font FONT_STAGE_INFO = new Font(Font.DIALOG, Font.PLAIN, 16);
+    public static final String[] SLIDER_VALUE_DESCRIPTION = {"Unacceptable", "Poor", "Not too good", "Average", "Quite good", "Impressive", "Amazing!"};
     public static final String PRESETS_HOWTO = "Adjust sliders accordingly to their importance.\n" +
             "Higher slider value represents higher multiplier for its item score.\n" +
             "If a slider is set to '0' - its item will not occur during the process.\n" +
             "You can choose one of default presets or create your own - adjust sliders and press 'Save Presets' button.\n" +
             "Press 'Start' button when you're ready, set the name for the process and start the evaluation.\n";
 
-    public static final String RESUME_STAGE_INFO = "resume stage";
+    public static final String RESUME_STAGE_INFO =
+            "<html>This stage's purpose is to evaluate candidate's background - whether it's beneficial for the applied position.<br>" +
+            "( It should be performed before the actual interview. )<br>" +
+            "<br>" +
+            "<br>" +
+            "At first check the candidate's resume (and application letter if attached). Consider the following:<br>" +
+            "- does it look neat and well prepared?<br>" +
+            "- are there spelling or grammar errors?<br>" +
+            "- does it contain all the necessary information?<br>" +
+            "- is it too long or too short?<br>" +
+            "- is it a default template or is it customized for the offer/company?<br>" +
+            "- does it look appealing for you as a recruiter?<br>" +
+            "<br>" +
+            "<br>" +
+            "Then you can check candidate's social media and attached links for website profiles or blogs.<br>" +
+            "- does the candidate's profile look interesting? Are you excited for the interview or the exact opposite?<br>" +
+            "- did you find any disturbing information about the candidate?<br>" +
+            "- do you think this candidate would fit well to the team?<br>" +
+            "<br></html>";
     public static final String LANGUAGE_STAGE_INFO = "language stage";
     public static final String EXPERIENCE_STAGE_INFO = "experience stage";
     public static final String PROJECTS_STAGE_INFO = "projects stage";
--- src/main/java/view/swing/stages/ResumeStagePanel.java
+++ src/main/java/view/swing/stages/ResumeStagePanel.java
@@ -3,11 +3,24 @@ package view.swing.stages;
 import view.swing.ViewConstants;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.util.HashMap;
 
 public class ResumeStagePanel extends JPanel implements Collectable {
 
     private StageView stageView;
+    private JLabel scoreLabel;
+    JSlider scoreSlider;
+    private final int POSITION_X = 50;
+    private final int INITIAL_POSITION_Y = 0;
+    private final int SCORE_LABEL_POSITION_Y = 520;
+    private final int TITLE_WIDTH = 500;
+    private final int TITLE_HEIGHT = 50;
+    private final int INFO_WIDTH = 900;
+    private final int INFO_HEIGHT = 500;
+    private final int SLIDER_WIDTH = 800;
+    private final int SLIDER_HEIGHT = 40;
 
     public ResumeStagePanel(StageView view) {
         this.stageView = view;
@@ -16,14 +29,71 @@ public class ResumeStagePanel extends JPanel implements Collectable {
 
     private void init(){
         setLayout(null);
+        add(createTitleLabel());
+        add(createInfoLabel());
+        add(createScoreSlider());
+        add(createScoreLabel());
+    }
 
+    private JLabel createTitleLabel(){
+        JLabel title = new JLabel("Resume & Social Media Evaluation Stage");
+        title.setFont(ViewConstants.FONT_LARGE);
+        title.setBounds(POSITION_X,INITIAL_POSITION_Y,TITLE_WIDTH,TITLE_HEIGHT);
+        return title;
+    }
+
+    private JLabel createInfoLabel(){
         JLabel info = new JLabel(ViewConstants.RESUME_STAGE_INFO);
-        info.setBounds(100,100,100,100);
-        add(info);
+        info.setFont(ViewConstants.FONT_STAGE_INFO);
+
+        int positionY = INITIAL_POSITION_Y + TITLE_HEIGHT;
+        info.setBounds(POSITION_X,positionY,INFO_WIDTH,INFO_HEIGHT);
+        return info;
+    }
+
+    private JSlider createScoreSlider(){
+        scoreSlider = new JSlider();
+        scoreSlider.setName("resumeSlider");
+        int positionX = POSITION_X + POSITION_X;
+        int positionY = INITIAL_POSITION_Y + TITLE_HEIGHT + INFO_HEIGHT;
+        scoreSlider.setBounds(positionX,positionY,SLIDER_WIDTH,SLIDER_HEIGHT);
+        scoreSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                updateScoreLabel(scoreSlider.getValue());
+            }
+        });
+
+        return scoreSlider;
+    }
+
+    private JLabel createScoreLabel(){
+        scoreLabel = new JLabel("Move the slider to evaluate candidate's performance at this stage:");
+        scoreLabel.setBounds(POSITION_X, SCORE_LABEL_POSITION_Y, INFO_WIDTH, SLIDER_HEIGHT);
+
+        return scoreLabel;
+    }
+
+    private void updateScoreLabel(int sliderValue){
+        String description = "";
+
+        if (sliderValue < 15) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[0];
+        else if (sliderValue >= 15 && sliderValue < 30) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[1];
+        else if (sliderValue >= 30 && sliderValue < 45) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[2];
+        else if (sliderValue >= 45 && sliderValue < 60) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[3];
+        else if (sliderValue >= 60 && sliderValue < 75) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[4];
+        else if (sliderValue >= 75 && sliderValue < 90) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[5];
+        else if (sliderValue >= 90) description = ViewConstants.SLIDER_VALUE_DESCRIPTION[6];
+
+        scoreLabel.setText("Current proficiency: " + description);
     }
 
     @Override
     public HashMap<String, String> collectData() {
-        return null;
+        HashMap<String, String> resumeScore = new HashMap<>();
+        int value = scoreSlider.getValue();
+        resumeScore.put("resume", String.valueOf(value));
+
+        return resumeScore;
     }
 }
