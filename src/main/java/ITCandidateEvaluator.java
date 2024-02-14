5fecaa697dbca1b5d474daf4286e7872a65f5e04
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-02-14 16:01:50 +0800

    Loading default presets from json files now works

--- src/main/java/ITCandidateEvaluator.java
+++ src/main/java/ITCandidateEvaluator.java
@@ -1,9 +1,12 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import controller.Controller;
 import model.*;
 import view.swing.View;
 
 import javax.swing.*;
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class ITCandidateEvaluator {
     public static void main(String[] args) {
@@ -13,38 +16,62 @@ public class ITCandidateEvaluator {
 //
 //        SwingUtilities.invokeLater(() ->{view.initView();});
 
-        HashMap<String, Integer> testMap = new HashMap<>();
-        testMap.put("Resume and social media evaluation", 1);
-        testMap.put("English language assessment", 8);
-        testMap.put("Previous work experience", 10);
-        testMap.put("Own projects", 10);
-        testMap.put("Live coding", 7);
-        testMap.put("Technical questions", 10);
-        testMap.put("Salary expectations", 7);
-        testMap.put("Soft skills", 10);
+//        HashMap<String, Integer> testMap = new HashMap<>();
+//        testMap.put("Resume and social media evaluation", 1);
+//        testMap.put("English language assessment", 8);
+//        testMap.put("Previous work experience", 10);
+//        testMap.put("Own projects", 10);
+//        testMap.put("Live coding", 7);
+//        testMap.put("Technical questions", 10);
+//        testMap.put("Salary expectations", 7);
+//        testMap.put("Soft skills", 10);
+//
+//        Presets presets = new Presets("testowe", testMap);
+//        Recruitment recruitment = model.createNewRecruitment("testowa", presets);
+//        int maxScore = recruitment.calculateMaxPossibleScore();
+//
+//        HashMap<Stages, Integer> scores = new HashMap<>();
+//        scores.put(Stages.RESUME, 10);
+//        scores.put(Stages.LANGUAGE, 85);
+//        scores.put(Stages.EXPERIENCE, 99);
+//        scores.put(Stages.PROJECTS, 90);
+//        scores.put(Stages.LIVE_CODING, 88);
+//        scores.put(Stages.QUESTIONS, 77);
+//        scores.put(Stages.SALARY, 111);
+//        scores.put(Stages.SOFT_SKILLS, 1);
+//
+//        Candidate candidate = new Candidate(recruitment, "John", "Smith");
+//        candidate.setScores(scores);
+//        int finalScore = recruitment.calculateFinalCandidateScorePercent(candidate);
+//        System.out.println("max score: " + maxScore);
+//        System.out.println(finalScore + "%");
+//        int ratio = recruitment.calculateCostValueRatio(candidate);
+//        System.out.println(ratio + "% salary");
 
-        Presets presets = new Presets("testowe", testMap);
-        Recruitment recruitment = model.createNewRecruitment("testowa", presets);
-        int maxScore = recruitment.calculateMaxPossibleScore();
+//        HashMap<String, Integer> seniorDefaultPresets = new HashMap<>();
+//        seniorDefaultPresets.put("Resume and social media evaluation", 5);
+//        seniorDefaultPresets.put("Previous work experience", 10);
+//        seniorDefaultPresets.put("Own projects", 10);
+//        seniorDefaultPresets.put("Technical questions", 7);
+//        seniorDefaultPresets.put("Salary expectations", 10);
+//        seniorDefaultPresets.put("Soft skills", 5);
+//
+//        String filePath = "src/main/resources/presets/senior_presets.json";
+//
+//        // Serialize the HashMap to JSON
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            mapper.writeValue(new File(filePath), seniorDefaultPresets);
+//            System.out.println("JSON file created successfully.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        HashSet<Presets> list = Presets.loadPresetsFromDirectory();
+        for (Presets presets : list) {
+            System.out.println(presets);
+        }
 
 
-
-        HashMap<Stages, Integer> scores = new HashMap<>();
-        scores.put(Stages.RESUME, 10);
-        scores.put(Stages.LANGUAGE, 85);
-        scores.put(Stages.EXPERIENCE, 99);
-        scores.put(Stages.PROJECTS, 90);
-        scores.put(Stages.LIVE_CODING, 88);
-        scores.put(Stages.QUESTIONS, 77);
-        scores.put(Stages.SALARY, 111);
-        scores.put(Stages.SOFT_SKILLS, 1);
-
-        Candidate candidate = new Candidate(recruitment, "John", "Smith");
-        candidate.setScores(scores);
-        int finalScore = recruitment.calculateFinalCandidateScorePercent(candidate);
-        System.out.println("max score: " + maxScore);
-        System.out.println(finalScore + "%");
-        int ratio = recruitment.calculateCostValueRatio(candidate);
-        System.out.println(ratio + "% salary");
     }
 }
--- src/main/java/model/Presets.java
+++ src/main/java/model/Presets.java
@@ -1,16 +1,46 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Presets {
     private String name;
     private HashMap<Stages, Integer> presets;
+    private static HashSet<Presets> defaultPresets;
+    private static Path presetsFilesDirectory = Paths.get("src/main/resources/presets");
 
     public Presets(String name, HashMap<String, Integer> modifiersValues) {
         this.name = name;
         presets = new HashMap<>();
         setPresets(modifiersValues);
+    }
+
+    public static HashSet<Presets> loadPresetsFromDirectory() {
+        final String[] fileName = new String[1];
+        defaultPresets = new HashSet<>();
+
+        try (Stream<Path> paths = Files.list(presetsFilesDirectory)) {
+            paths.filter(file -> file.getFileName().toString().contains("presets") && file.toString().endsWith(".json"))
+                    .forEach(file -> {
+                        ObjectMapper mapper = new ObjectMapper();
+                        try {
+                            fileName[0] = file.getFileName().toString().replaceAll(".json", "");
+                            HashMap<String, Integer> map = mapper.readValue(file.toFile(), HashMap.class);
+                            defaultPresets.add(new Presets(fileName[0], map));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return defaultPresets;
     }
 
     public void setPresets(HashMap<String, Integer> modifiersValues){
@@ -28,5 +58,12 @@ public class Presets {
 
     public HashMap<Stages, Integer> getPresets() {
         return presets;
+    }
+
+    @Override
+    public String toString() {
+        return "Presets{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
